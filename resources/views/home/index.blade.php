6fc88d4cf3318c876e7ee122db7a2857b20ca8ee
moosenado <egrabish@gmail.com>
2015-10-12 11:10:33 -0400

    list styles

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,46 +12,46 @@
 
 	<div class="row">
 
-		<body class="park-list text-center">
+		<body class="park-list">
 
 			<ul>
-				<li>All Parks</li>
-				<li>Amphitheatre</li>
-				<li>Auditorium</li>
-				<li>Ball Diamond</li>
-				<li>Clubhouse</li>
-				<li>Coat Check Room</li>
-				<li>Computer/Training Room</li>
-				<li>Craft Room</li>
-				<li>Curling Rink</li>
-				<li>Dogs Off-Leash Area</li>
-				<li>Dressing Room</li>
-				<li>Firepit</li>
-				<li>Fitness/Weight Room</li>
-				<li>Gallery</li>
-				<li>Games Room</li>
-				<li>Gymnasium</li>
-				<li>Indoor Dry Pad</li>
-				<li>Indoor Pool</li>
-				<li>Indoor Rink</li>
-				<li>Indoor Track</li>
-				<li>Kitchen</li>
-				<li>Multipurpose Room</li>
-				<li>Outdoor Dry Pad</li>
-				<li>Outdoor Pool</li>
-				<li>Outdoor Rink</li>
-				<li>Outdoor Track</li>
-				<li>Park Shelter</li>
-				<li>Playground</li>
-				<li>Pool</li>
-				<li>ProShop</li>
-				<li>Skateboard Park</li>
-				<li>Splash Pad</li>
-				<li>Sport Field</li>
-				<li>Stadium</li>
-				<li>Stage</li>
-				<li>Wading Pool</li>
-				<li>Washroom</li>
+				<li><p>All Parks</p></li>
+				<li><p>Amphitheatre</p></li>
+				<li><p>Auditorium</p></li>
+				<li><p>Ball Diamond</p></li>
+				<li><p>Clubhouse</p></li>
+				<li><p>Coat Check Room</p></li>
+				<li><p>Computer/Training Room</p></li>
+				<li><p>Craft Room</p></li>
+				<li><p>Curling Rink</p></li>
+				<li><p>Dogs Off-Leash Area</p></li>
+				<li><p>Dressing Room</p></li>
+				<li><p>Firepit</p></li>
+				<li><p>Fitness/Weight Room</p></li>
+				<li><p>Gallery</p></li>
+				<li><p>Games Room</p></li>
+				<li><p>Gymnasium</p></li>
+				<li><p>Indoor Dry Pad</p></li>
+				<li><p>Indoor Pool</p></li>
+				<li><p>Indoor Rink</p></li>
+				<li><p>Indoor Track</p></li>
+				<li><p>Kitchen</p></li>
+				<li><p>Multipurpose Room</p></li>
+				<li><p>Outdoor Dry Pad</p></li>
+				<li><p>Outdoor Pool</p></li>
+				<li><p>Outdoor Rink</p></li>
+				<li><p>Outdoor Track</p></li>
+				<li><p>Park Shelter</p></li>
+				<li><p>Playground</p></li>
+				<li><p>Pool</p></li>
+				<li><p>ProShop</p></li>
+				<li><p>Skateboard Park</p></li>
+				<li><p>Splash Pad</p></li>
+				<li><p>Sport Field</p></li>
+				<li><p>Stadium</p></li>
+				<li><p>Stage</p></li>
+				<li><p>Wading Pool</p></li>
+				<li><p>Washroom</p></li>
 			</ul>
 
 		</body>
