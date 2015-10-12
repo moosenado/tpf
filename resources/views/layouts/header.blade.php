4545827b410b8a659a61f53963de4295a19edbfb
moosenado <egrabish@gmail.com>
2015-10-12 11:32:49 -0400

    style update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" >
+		<link href='http://fonts.googleapis.com/css?family=Archivo+Narrow:400,700italic,700,400italic' rel='stylesheet' type='text/css'>
 	</head>
 
 	<body>
