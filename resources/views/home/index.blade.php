eaf4229f9d9f52ae70b8fee23f9a7e444311cd70
moosenado <egrabish@gmail.com>
2015-10-18 10:58:58 -0400

    checkmark addition

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -32,6 +32,7 @@
 
 					<li id="park-<?php echo $counter++ ?>" class="li-bg" data-parkval="<?php echo $park_val ?>" data-parkvaloffish="<?php echo $park->facility ?>" data-selected="0">
 						<p> <?php echo $park->facility ?> </p>
+						<object class="svg-check" type="image/svg+xml" data="https://upload.wikimedia.org/wikipedia/commons/7/71/Ok_sign_font_awesome.svg"></object>
 					</li>
 
 				<?php endforeach; ?>
