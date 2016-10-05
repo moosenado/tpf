550c4c10487782e10cfc86742a008d8c6e2c3759
moosenado <egrabish@gmail.com>
2016-10-05 19:29:22 -0400

    correct color

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="theme-color" content="#ffffff">
+	<meta name="theme-color" content="#71e271">
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" >
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href='https://fonts.googleapis.com/css?family=Archivo+Narrow:400,700italic,700,400italic' rel='stylesheet' type='text/css'>
