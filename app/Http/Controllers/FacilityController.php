87a98cceeac7d7572d86748970e3c3bda34f6e57
moosenado <egrabish@gmail.com>
2016-10-23 11:04:08 -0400

    add caching to facility choices

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -7,6 +7,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Facility;
 use App\Http\Requests\GetFacilitiesRequest;
+use Illuminate\Support\Facades\Cache;
 
 class FacilityController extends Controller
 {
@@ -14,34 +15,44 @@ class FacilityController extends Controller
     {
         $data = $request->all();
 
-    	$query = new Facility();
-        if (!empty($data['facilities'])) {
-            $parks_data = explode(',', $data['facilities']);
-
-            foreach($parks_data as $park) {
-                $query = $query->where(trim($park), true);
-            }
-            $facilities = $query->get();
+        if (Cache::store('database')->has($data['facilities']))
+        {
+            $facilities = Cache::store('database')->get($data['facilities']);
         }
         else
         {
-            $facilities = $query->all();
+            $query = new Facility();
+
+            if (!empty($data['facilities'])) {
+
+                $parks_data = explode(',', $data['facilities']);
+
+                foreach($parks_data as $park) {
+                    $query = $query->where(trim($park), true);
+                }
+
+                $facilities = $query->get();
+            }
+            else
+            {
+                $facilities = $query->all();
+            }
+
+            if ($facilities->isEmpty()) {
+                throw new \Exception('No facilities match criteria');
+            }
+            else
+            {
+                Cache::store('database')->put($data['facilities'], $facilities, 10);
+            }
         }
-        
-    	$facilities = $query->get();
 
         $your_location = array();
         $your_location[] = trim($data['lat']);
         $your_location[] = trim($data['lng']);
 
-        
         $facilities = Facility::sortFacilitiesByDistance($facilities, $your_location);
-
-        if (!$facilities) {
-            throw new \Exception('No facilities match criteria');
-        }
         
-
     	return $facilities;
     }
 
